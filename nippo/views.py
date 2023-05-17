c3a3a8cce3128ceb345b6652342616e73da86012
namor <n2210478@gl.cc.uec.ac.jp>
2023-05-17 18:09:26 +0900

    2-5

--- nippo/views.py
+++ nippo/views.py
@@ -14,3 +14,14 @@ def nippoDetailView(request, number):
         "number": number,
     }
     return render(request, template_name, ctx)
+
+def nippoCreateView(request):
+    template_name="nippo/nippo-form.html"
+
+    if request.POST:
+        title = request.POST["title"]
+        content = request.POST["content"]
+
+    #受け取った値で必要な処理を行います
+        
+    return render(request, template_name)
